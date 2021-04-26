1cb1ec24409ee3edebd3cac6fd1e7ca6d880176b
romantsinevich <braid98@yandex.ru>
2021-04-26 13:42:35 +0300

    Refactor popup

--- src/Landmark.tsx
+++ src/Landmark.tsx
@@ -1,11 +1,28 @@
 import L from "leaflet";
 import landmarkIcon from "./landmark.png";
-import {Marker} from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
 import React from "react";
+import questIcon from "./quest.png";
 
 export const Landmark = (props: any) => {
     const landmark = L.icon({
         iconUrl: landmarkIcon,
     });
-    return <Marker position={props.latlng} icon={landmark}/>
+    return (
+        <Marker position={props.latlng} icon={landmark}>
+            <Popup>
+                <div>
+                    <img alt='something' src={questIcon} width='150' height='150'/>
+                    <div>
+                        <h1>{props.name}</h1>
+                        <span>30 min</span>
+                        <span>30 min</span>
+                        <span>30 min</span>
+                    </div>
+                    <button>start</button>
+                    <button>more</button>
+                </div>
+            </Popup>
+        </Marker>
+    )
 };
--- src/Quest.tsx
+++ src/Quest.tsx
@@ -1,25 +1,28 @@
 import L from "leaflet";
 import questIcon from "./quest.png";
-import {Marker, Popup} from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
 import React from "react";
+import { sendMessage } from "./message-service";
 
 export const Quest = (props: any) => {
     const quest = L.icon({
         iconUrl: questIcon,
     });
-    return <Marker position={props.latlng} icon={quest}>
-        <Popup>
-            <div>
-                <img alt='something' src={questIcon} width='150' height='150'/>
+    return (
+        <Marker position={props.latlng} icon={quest}>
+            <Popup>
                 <div>
-                    <h1>YOOOOOOO</h1>
-                    <span>30 min</span>
-                    <span>30 min</span>
-                    <span>30 min</span>
+                    <img alt='something' src={questIcon} width='150' height='150'/>
+                    <div>
+                        <h1>YOOOOOOO</h1>
+                        <span>30 min</span>
+                        <span>30 min</span>
+                        <span>30 min</span>
+                    </div>
+                    <button>start</button>
+                    <button onClick={() => sendMessage('QUEST', props.id)}>more</button>
                 </div>
-                <button>start</button>
-                <button>more</button>
-            </div>
-        </Popup>
-    </Marker>
+            </Popup>
+        </Marker>
+    )
 };
